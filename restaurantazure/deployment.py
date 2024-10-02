b5865ce7db21cb6dabee7e50cb1e421c83f1197d
Ilya Vlasyuk <ilyavlasyuk84@gmail.com>
2024-10-02 23:44:34 +0300

    doljno rabotat ya proveril

--- restaurantazure/deployment.py
+++ restaurantazure/deployment.py
@@ -4,8 +4,8 @@ from .settings import BASE_DIR
 import whitenoise
 
 
-SECRET_KEY = os.environ["SECRET"]
-ALLOWED_HOSTS = ['azurerestaurant.azurewebsites.net']
+SECRET_KEY = '12345'
+ALLOWED_HOSTS = ['azurerestaurant.azurewebsites.net', 'localhost', '127.0.0.1']
 CSRF_TRUSTED_ORIGINS = ['https://' + 'azurerestaurant.azurewebsites.net']
 DEBUG = False
 
@@ -20,5 +20,40 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+import os
+import logging
+import logging.config
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '{levelname} {asctime} {module} {message}',
+            'style': '{',
+        },
+        'simple': {
+            'format': '{levelname} {message}',
+            'style': '{',
+        },
+    },
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',  # Уровень логирования
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(BASE_DIR, 'django_debug.log'),  # Путь к файлу логов
+            'formatter': 'verbose',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
